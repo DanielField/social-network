b261f9fdfe352fd7ce768fb66fcbad5d753aeaae
Daniel Field <danielfield3515@gmail.com>
2019-11-15 08:13:04 +1000

    fixed confirmAdmin query

--- authenticate.js
+++ authenticate.js
@@ -31,7 +31,7 @@ var confirmAdmin = async function(token) {
     if (token == null || token == undefined) return 0;
     try {
         var decoded_token = jwt.verify(token, process.env.SECRET_KEY);
-        var result = await database.query(`SELECT user_id FROM users WHERE user_id=${decoded_token.user_id};`);
+        var result = await database.query(`SELECT user_id,is_admin FROM users WHERE user_id=${decoded_token.user_id};`);
         if (result) return (result[0].is_admin) ? 1 : 0;
     } catch(err) {
         console.log(err);
